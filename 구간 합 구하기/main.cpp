62d83455292c40579e5dc89035ad35471b5194c5
woongja <wcn4457@gmail.com>
2023-05-23 10:47:50 +0900

    Initial Commit

---
+++ 구간 합 구하기/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  구간 합 구하기
+//
+//  Created by 이웅재 on 2023/05/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
