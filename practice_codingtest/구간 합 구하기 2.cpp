0fa3ea6c045846666ec9ef1b5892b957416363b6
woongja <wcn4457@gmail.com>
2023-05-23 19:12:56 +0900

    구간 합 구하기 2

---
+++ practice_codingtest/구간 합 구하기 2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    
+    int N,M;
+    int x1,x2;
+    int y1,y2;
+    
+    cin >> N >> M;
+    
+    vector<vector<int>> A(N+1,vector<int>(N+1,0));
+    vector<vector<int>> D(N+1,vector<int>(N+1,0));
+    
+    for(int i = 1; i<=N; i++){
+        for(int j=1; j<=N; j++){
+            cin >> A[i][j];
+            
+            D[i][j] = D[i][j-1] + D[i-1][j] - D[i-1][j-1] + A[i][j];
+        }
+    }
+    
+    for(int i=0; i<M; i++){
+        cin >> x1 >>y1 >> x2 >> y2;
+        
+        int result = D[x2][y2] - D[x1-1][y2] - D[x2][y1-1] + D[x1-1][y1-1];
+        
+        cout << result << endl;
+        
+    }
+    
+    
+}
